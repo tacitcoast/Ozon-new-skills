dc2efdae7186f4692d7487114edc93668439612c
Anna Malinovskaia <44261093+tacitcoast@users.noreply.github.com>
2021-03-23 22:05:38 +0300

    Add files via upload

---
+++ Guess_number_game.py
@@ -0,0 +1,16 @@
+import random
+
+while True:
+  rand_value = random.randint(1, 30)
+
+  while True:
+    guess = int(input('Угадай число: '))
+    print(guess)
+
+    if rand_value == guess:
+      print('Молодец! Ты победил!')
+      break
+    elif rand_value > guess:
+      print('Возьми число побольше')
+    else:
+      print('Возьми число поменьше')
